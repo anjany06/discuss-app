cb474f4a739d137702540cf40d0d319f900620df
Anjany Pandey <anjany.pandey06@gmail.com>
2025-02-22 18:56:55 +0530

    NextAuth Usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { signIn } from "./actions/sign-in";
+import { signOut } from "./actions/sign-out";
 
 export default function Home() {
   return (
-  <div>
-    <h1>Home</h1>
-    <Button>new</Button>
-  </div>
+    <div>
+      <h1>Home</h1>
+      <form action={signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+      <form action={signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+    </div>
   );
 }
