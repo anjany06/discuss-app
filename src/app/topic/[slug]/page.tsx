a390390ff9459af892967d0c445c784e24ce22fe
Anjany Pandey <anjany.pandey06@gmail.com>
2025-02-23 17:33:53 +0530

    feat: post creation done with error handling

--- src/app/topic/[slug]/page.tsx
+++ src/app/topic/[slug]/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import PostCreateForm from "@/components/posts/PostCreateForm";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
-const TopicShowPage = () => {
+type TopicShowPageProps = {
+  params: Promise<{ slug: string }>;
+};
+
+const TopicShowPage: React.FC<TopicShowPageProps> = async ({ params }) => {
+  const slug = (await params).slug;
   return (
-    <div>TopicShowPage</div>
-  )
-}
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1>{slug}</h1>
+      </div>
+      <div>
+        <PostCreateForm slug={slug} />
+      </div>
+    </div>
+  );
+};
 
 export default TopicShowPage;
